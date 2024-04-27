495d8a0d7938b4d811d7bf17901a7c626ca771ec
Lohith <69358508+lohithgsk@users.noreply.github.com>
2024-04-27 18:06:29 +0530

    First Commit

---
+++ index.py
@@ -0,0 +1,74 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from pymongo import MongoClient, InsertOne
+import json
+from pprint import pprint
+
+
+app = Flask(__name__)
+app.secret_key = 'groptimizer'
+app.config['MESSAGE_FLASHING_OPTIONS'] = {'duration': 5}
+
+client = MongoClient("mongodb+srv://loki:loki@cluster0.t4mij2c.mongodb.net/")
+db = client["Groptimizer"]
+register_collection = db["Register"]
+
+@app.route("/", methods=["GET"])
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+
+        option = request.form['options']
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if len(list(register_collection.find({"username": username, "option": option}))) == 0:
+            flash('User does not exist','error')
+            return render_template('login.html')
+
+        for user_record in register_collection.find({"username": username, "option":option}):
+            if check_password_hash(user_record['password'], password):
+                flash("Logged-In Successfully",'success')
+                return render_template("home.html")
+            else:
+                flash('Invalid Password','error')
+                return render_template('login.html')
+    else:
+        return render_template('login.html')
+    
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+
+        option = request.form['options']
+        name = request.form.get("name")
+        address = request.form.get("address")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        phno = request.form.get("phno")
+        website = request.form.get("website")
+        password = request.form.get("password")
+
+        user = {
+            "option": option,
+            "name": name,
+            "address": address,
+            "username": username,
+            "email": email,
+            "phno": phno,
+            "website": website,
+            "password": generate_password_hash(password)
+        }
+
+        register_collection.insert_one(user)
+        
+        flash('Registered Successfully', 'success')
+        return render_template("home.html")
+    else:
+        return render_template("register.html")
